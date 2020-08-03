3eac1143605192d6eb350ba2e73e598371df761c
EqualMa <equalma@outlook.com>
2020-08-04 01:33:31 +0800

    chore: setup project

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/env",
+      {
+        targets: {
+          node: "8",
+        },
+        useBuiltIns: "usage",
+        corejs: {
+          version: 3,
+          // proposals: true,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    //
+    "@babel/plugin-proposal-class-properties",
+  ],
+};
