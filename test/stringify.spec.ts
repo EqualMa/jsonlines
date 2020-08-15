d74e4527d977c8ed8f1980dbdceb3da14573857e
EqualMa <equalma@outlook.com>
2020-08-15 18:21:58 +0000

    test: stringify nullValue

--- test/stringify.spec.ts
+++ test/stringify.spec.ts
@@ -1,4 +1,4 @@
-import { stringify } from "../src";
+import { stringify, nullValue } from "../src";
 import { pipeline as _pl, Readable } from "stream";
 import { promisify } from "util";
 import { streamToString } from "../src/util/stream-to";
@@ -27,6 +27,40 @@ test("stringify objects to jsonlines", async () => {
   expect(jsonlinesText).toBe(
     values.map((v) => JSON.stringify(v)).join("\n") + "\n",
   );
+
+  await done;
+});
+
+test("fail on null value", async () => {
+  const values = [null];
+  const source = Readable.from(values);
+  const duplex = stringify();
+
+  const done = pipeline(source, duplex);
+  duplex.read();
+
+  try {
+    await done;
+    expect("unreachable code").toBeUndefined();
+  } catch (err) {
+    expect((err as { code: unknown }).code).toBe("ERR_STREAM_NULL_VALUES");
+  }
+});
+
+test("stringify nullValue", async () => {
+  const values = [
+    //
+    nullValue,
+  ];
+
+  const source = Readable.from(values);
+  const duplex = stringify();
+
+  const done = pipeline([source, duplex]);
+
+  const jsonlinesText = await streamToString(duplex, "utf8");
+
+  expect(jsonlinesText).toBe("null\n");
 
   await done;
 });
